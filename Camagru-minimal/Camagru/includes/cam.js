53464179b620a83ee6262ab5b26d95690674766a
Brandon SWANEPOEL <bswanepo@c2r3s1.wethinkcode.co.za>
2019-11-20 10:42:47 +0200

    Changed Stickers

--- Camagru-minimal/Camagru/includes/cam.js
+++ Camagru-minimal/Camagru/includes/cam.js
@@ -1,4 +1,3 @@
-
 var video = document.querySelector("#vid");
 var overlay = document.querySelectorAll(".supers");
 
@@ -6,12 +5,11 @@ var overlay = document.querySelectorAll(".supers");
 var canv = document.getElementById("uploadCanvas");
 var contx = canv.getContext('2d');
 var image = new Image();
+image.setAttribute("crossorgin","anonymous");
+
 
 //set video from new_upload to stream from webcam
 navigator.mediaDevices.getUserMedia({video:true}).then(function(stream) {
-    
-    video.crossorigin = "Anonymous";
-
         video.srcObject = stream;
         video.play();
     }).catch(function(error) {
@@ -24,31 +22,31 @@ document.getElementById('shoot').addEventListener('click', function() {
         document.getElementById('submit_taken').setAttribute("class", "button is-centered");
     });
 
-//upload taken pic on click of submit button
-document.getElementById("submit_taken").addEventListener('click', function() {
-    var dataURL = canv.toDataURL('image/png');
-    document.getElementById('taken').value = dataURL;
-});
-
-
 //choose edit overlays
 overlay.forEach(function(element) {
     element.addEventListener('click', function(){
         image = element;
         if (image.src === "http://localhost:8080/Camagru/images/frame.png") {
+
             contx.drawImage(image, 0, 0, 720, 480);
         }
         if (image.src === "http://localhost:8080/Camagru/images/f-sticker.png") {
             contx.drawImage(image, 500, 100, 300, 330);
         }
-        if (image.src === "http://localhost:8080/Camagru/images/cat-sticker.png") {
+        if (image.src === "http://localhost:8080/Camagru/images/h-sticker.png") {
             contx.drawImage(image, 0, 45, 300, 250);
         }
-        if (image.src === "http://localhost:8080/Camagru/images/h-sticker.png") {
+        if (image.src === "http://localhost:8080/Camagru/images/cat-sticker.png") {
             contx.drawImage(image, 0, 280, 255, 200);
         }
         if (image.src === "http://localhost:8080/Camagru/images/q-sticker.png") {
             contx.drawImage(image, 140, 45, 380, 380);
     }
     });
+});
+
+//upload taken pic on click of submit button
+document.getElementById("submit_taken").addEventListener('click', function() {
+    var dataURL = canv.toDataURL('image/png');
+    document.getElementById('taken').value = dataURL;
 });
